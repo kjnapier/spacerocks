914fc6fb938fc6960d5b9061fab65adf24520c30
kjnapes <kjnapier@umich.edu>
2020-05-12 14:01:32 -0400

    pushing orbfit, simulator

---
+++ test_spacerocks.py
@@ -0,0 +1,25 @@
+import unittest
+import spacerocks
+
+class TestSpacerocks(unittest.TestCase):
+
+    def test_xyz_to_kep(self):
+        self.assertAlmostEqual()
+
+    def test_kep_to_xyz(self):
+        self.assertAlmostEqual()
+
+    def test_xyz_to_equa(self):
+        self.assertAlmostEqual()
+
+    def test_radec_to_hpix(self):
+        self.assertAlmostEqual()
+
+    def test_helio_to_bary(self):
+        self.assertAlmostEqual()
+
+    def test_bary_to_helio(self):
+        self.assertAlmostEqual()
+
+if __name__ == '__main__':
+    unittest.main()
