4688dd86045b79461d4d181fa9fb9f061ded440f
kjnapes <kjnapier@umich.edu>
2020-05-17 01:37:52 -0400

    restructuring

---
+++ build/lib/spacerocks/linalg3d.py
@@ -0,0 +1,28 @@
+from numba import jit
+import numpy as np
+# Optimized pure-Python vector algebra in three dimensions.
+### for some reason, jit destroys astropy units. I don't think the speedup is worth the sacrifice.
+# jit
+def cross(a, b):
+    x1 = a[1] * b[2] - a[2] * b[1]
+    x2 = a[2] * b[0] - a[0] * b[2]
+    x3 = a[0] * b[1] - a[1] * b[0]
+    return x1, x2, x3
+
+# @jit
+def dot(a, b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+# @jit
+def norm(a):
+    return (a[0]**2 + a[1]**2 + a[2]**2)**0.5
+
+@jit
+def euler_rotation(a, b, c, x):
+    # Currently assumes z=0
+    xrot = x[0] * (np.cos(a)*np.cos(c) - np.sin(a)*np.sin(c)*np.cos(b)) - \
+           x[1] * (np.sin(a)*np.cos(c) + np.cos(a)*np.sin(c)*np.cos(b))
+    yrot = x[0] * (np.cos(a)*np.sin(c) + np.sin(a)*np.cos(c)*np.cos(b)) + \
+           x[1] * (np.cos(a)*np.cos(c)*np.cos(b) - np.sin(a)*np.sin(c))
+    zrot = x[0] * (np.sin(a)*np.sin(b)) + x[1] * (np.cos(a)*np.sin(b))
+    return xrot, yrot, zrot
--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
    name='spacerocks',
-   version='0.0.0',
+   version='0.4',
    description='Calculate solar system ephemerides from orbital elements.',
    author='Kevin Napier',
    author_email='kjnapier@umich.edu',
    url="https://github.com/kjnapes/spacerocks",
    packages=['spacerocks'],
-   install_requires=['healpy', 'numpy', 'matplotlib',
-                     'random', 'scipy', 'skyfield.api',
-                     'astropy', 'numba', 'pandas', 'sys']
+   install_requires=['healpy', 'numpy', 'matplotlib', 'scipy', 'skyfield',
+                     'astropy', 'numba', 'pandas']
 )
