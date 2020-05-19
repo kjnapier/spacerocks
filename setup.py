40c7d2e046c45857067b5b369ba3e1ed2a47dc41
kjnapes <kjnapier@umich.edu>
2020-05-19 14:14:30 -0400

    -> version 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='spacerocks',
-   version='0.5.2',
+   version='0.5.3',
    description='Calculate solar system ephemerides from orbital elements.',
    author='Kevin Napier',
    author_email='kjnapier@umich.edu',
