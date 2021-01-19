d02002b50a5d7b941c457dcbba7a992d0f00b454
kjnapes <kjnapier@umich.edu>
2021-01-19 14:41:58 -0500

    units

---
+++ spacerocks/units.py
@@ -0,0 +1,21 @@
+from astropy import units as u
+from astropy.table import Table
+
+class Units:
+
+    def __init__(self):
+        self.set_default()
+
+    def set_default(self):
+        self.distance = u.au
+        self.angle = u.deg
+        self.timescale = 'utc'
+        self.timeformat = None
+        self.speed = u.au / u.d
+        self.rotation_period = u.d
+
+    def current(self):
+        print("{:<20} {:<15}".format('Quantity', 'Unit'))
+        print('---------------------------------------')
+        for k, v in self.__dict__.items():
+            print("{:<20} {:<15}".format(k, str(v)))
