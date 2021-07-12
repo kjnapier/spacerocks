42e6c206979f00a69b512f8bb1a53ea7a7a4553f
kjnapes <kjnapier@umich.edu>
2021-07-12 16:44:10 -0400

    reintroducing unit tests

--- spacerocks/tests/test_spacerocks.py
+++ spacerocks/tests/test_spacerocks.py
@@ -1,27 +1,24 @@
-#import unittest
-#from spacerocks import SpaceRock, Propagate
+import unittest
+from spacerocks import SpaceRock, Units
 
-#BP = SpaceRock(a=4.487673062316562E+02,
-#               e=9.214543710796702E-01,
-#               inc=5.411068217470999E+01,
-#               arg=3.480587931444684E+02,
-#               node=1.352131434907198E+02,
-#               t_peri=2.473015776611103E+06,
-#               epoch=2458982.5,
-#               H=4.5,
-#               name='BP',
-#               input_coordinates='keplerian',
-#               input_frame='barycentric',
-#               input_angles='degrees',
-#               obscode=304)
-#
-#class TestSpacerocks(unittest.TestCase):
-#
-#    def test_ephemerides(self):
-#        self.assertAlmostEqual(BP.ra.rad[0], 1.1921542590992633)
-#        self.assertAlmostEqual(BP.dec.rad[0], -0.5464389502009863)
-#        self.assertAlmostEqual(BP.mag.value[0], 21.6360133068)
-#        self.assertAlmostEqual(BP.M.value[0], -0.0254097208)
-#
-#if __name__ == '__main__':
-#    unittest.main()
+units = Units()
+units.timescale = 'tdb'
+BP = SpaceRock(a=4.487673062316562E+02,
+               e=9.214543710796702E-01,
+               inc=5.411068217470999E+01,
+               arg=3.480587931444684E+02,
+               node=1.352131434907198E+02,
+               t_peri=2.473015776611103E+06,
+               epoch=2458982.5,
+               H0=4.5,
+               name='2015 BP519',
+               frame='barycentric',
+               units=units)
+
+class TestSpacerocks(unittest.TestCase):
+
+    def test_ephemerides(self):
+        self.assertAlmostEqual(BP.x.au[0], 1.581128776843651E+01)
+
+if __name__ == '__main__':
+    unittest.main()
