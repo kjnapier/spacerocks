e2b4c3998b8be2e3b541dfe88a6f4b188f8a8093
kjnapes <kjnapier@umich.edu>
2021-09-01 15:33:38 -0400

    M_PI error

--- src/speedy.c
+++ src/speedy.c
@@ -8,7 +8,7 @@ const double IMIN = 1e-8;
 const double mu_bary = 0.00029630927493457475;
 
 #ifndef M_PI
-    #define M_PI 3.14159265358979323846;
+    #define M_PI 3.14159265358979323846
 #endif
 
 struct StateVector{
