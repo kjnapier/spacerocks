ecbc6d59ff8d3dc870590c27468c16ab4d0c1f6d
kjnapes <kjnapier@umich.edu>
2021-09-06 23:33:35 -0400

    Working on SpiceBody

--- spacerocks/spicebody.py
+++ spacerocks/spicebody.py
@@ -2,48 +2,68 @@ from .units import Units
 from .spacerock import SpaceRock
 
 import spiceypy as spice
-
+from astropy import units as u
 
 class SpiceBody:
 
-    def __init__(self, spiceid, frame='ECLIPJ2000', origin='ssb'):
-        self.spiceid = spiceid
+    def __init__(self, frame='ECLIPJ2000', origin='ssb'):
+
         self.frame = frame
         self.origin = origin
-        
-    def at(self, epoch):
 
-        ephemeris_times = spice.str2et(['JD{} TDB'.format(epoch)])
-        state, _ = spice.spkezr(self.spiceid, ephemeris_times, self.frame, 'none', self.origin)
-        state = np.vstack(state).T
-        x, y, z, vx, vy, vz = state 
+        if kwargs.get('epoch') is not None:
+            self.epoch = np.atleast_1d(kwargs.get('epoch'))
 
-        units = Units()
-        units.distance = u.km
-        units.speed = u.km / u.s
-        
-        rocks = SpaceRock(x=x, 
-                          y=y, 
-                          z=z, 
-                          vx=vx, 
-                          vy=vy, 
-                          vz=vz, 
-                          units=units, 
-                          epoch=epoch, 
-                          origin=self.origin,
-                          frame=self.frame)
+        if len(kwargs.get('spiceid')) < len(self.epoch):
+                self.spiceid = np.repeat(kwargs.get('spiceid'), len(self.epoch))
+            else:
+                self.spiceid = np.atleast_1d(kwargs.get('spiceid'))
+
     
-class Earth(SpiceBody):
+    def __get_all_state_vectors(self):
+        '''
+        Very optimized way to get all state vectors from spice.
 
-    def topos(self, latitude, longitude, elevation):
-        pass
+        The code is sort of convoluted, but it works and is several 
+        times faster than any alternative without resorting to 
+        using CSpice directly (don't want to deal with that).
 
-    def rotation_model(self):
-        pass
+        1. set(list(zip(...))) gets unique (body, time) pairs.
 
-# class Planet(SpiceBody):
+        2. These pairs are used as the keys in a dictionary storing
+           the state vectors as numpy arrays. The key is also passed 
+           to the __state_from_spice function to calculate the state 
+           vector.
 
-#     # self.mass = mass
-#     # self.radius = radius
-#     # self.albedo = albedo
-#     pass
+        3. Use a list comprehension to populate a numpy array with 
+           all state vectors by reading from the dictionary. This is faster 
+           than numpy indexing. The time complexity is O(1).
+
+        4. Set attributes using the usual astropy units.
+
+        '''
+
+        unique_rocks_and_times = set(list(zip(self.spiceid, self.epoch)))
+        unique_dict = {key:self.__state_from_spice(key) for key in unique_rocks_and_times}
+        x, y, z, vx, vy, vz = np.array([unique_dict[(body, time)] for body, time in zip(self.spiceid, self.epoch)]).T
+       
+        self.x = Distance(x, u.km, allow_negative=True).to(u.au)
+        self.y = Distance(y, u.km, allow_negative=True).to(u.au)
+        self.z = Distance(z, u.km, allow_negative=True).to(u.au)
+
+        self.vx = (vx * u.km/u.s).to(u.au / u.day)
+        self.vy = (vy * u.km/u.s).to(u.au / u.day)
+        self.vz = (vz * u.km/u.s).to(u.au / u.day)
+
+
+    def __compute_ephemeris_time(self, epoch):
+        '''
+        Wrapper for spiceypy's str2et function.
+        '''
+        return spice.str2et('JD{} UTC'.format(epoch))
+
+    def __state_from_spice(self, x):
+        spiceid, epoch = x
+        ephemeris_time = self.__compute_ephemeris_time(epoch)
+        state, _ = spice.spkezr(spiceid, ephemeris_time, self.frame, 'none', self.origin)
+        return state
