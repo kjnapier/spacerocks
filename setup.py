7e4b869444c1980722b471fe982ef42a0b0cb8f9
kjnapes <kjnapier@umich.edu>
2021-09-15 12:37:28 -0400

    adding some package data to setup

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
                 'spacerocks/data/spice/2000001.bsp',
                 'spacerocks/data/spice/2000004.bsp',
                 'spacerocks/data/spice/de440s.bsp',
-                'spacerocks/data/spice/gm_de431.tcp',
+                'spacerocks/data/spice/gm_de431.tpc',
                 'spacerocks/data/spice/hst.bsp',
                 'spacerocks/data/spice/latest_leapseconds.tls',
                 'spacerocks/data/spice/test.bsp',
