1579837a854754460733c25caeb7ac87fa39e133
kjnapes <kjnapier@umich.edu>
2022-03-21 02:06:09 -0400

    Working on linux compilation string

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ suffix = sysconfig.get_config_var('EXT_SUFFIX')
 if suffix is None:
     suffix = ".so"
 
-extra_link_args = []
+extra_link_args = ['-lgomp']
 extra_compile_args = ['-O3', '-fPIC', '-std=gnu++2a', '-march=native', '-fopenmp']
 
 if sys.platform == 'darwin':
