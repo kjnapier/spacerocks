f18a13c8c34e75ed73a6366ef47503de3fd2f679
kjnapes <kjnapier@umich.edu>
2022-04-03 20:48:25 -0400

    refactoring data files

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
     data_files=data_files,
     include_package_data=True,
     install_requires=['healpy',
+                      'rich',
                       'asdf',
                       'numpy',
                       'spiceypy',
