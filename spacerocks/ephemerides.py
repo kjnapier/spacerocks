e2e438705d8c602b1b087d7fc69f9f3c34fe2bcc
kjnapes <kjnapier@umich.edu>
2022-04-04 14:05:59 -0400

    changing ephemeris

--- spacerocks/ephemerides.py
+++ spacerocks/ephemerides.py
@@ -18,9 +18,10 @@ from .spice import SpiceBody
 SPICE_PATH = pkg_resources.resource_filename('spacerocks', 'data/spice')
 spice.furnsh(os.path.join(SPICE_PATH, 'latest_leapseconds.tls'))
 #spice.furnsh(os.path.join(SPICE_PATH, 'de440s.bsp'))
-spice.furnsh(os.path.join(SPICE_PATH, 'de441.bsp'))
+#spice.furnsh(os.path.join(SPICE_PATH, 'de441.bsp'))
 #spice.furnsh(os.path.join(SPICE_PATH, 'hst.bsp'))
 spice.furnsh(os.path.join(SPICE_PATH, 'nh_pred_alleph_od151.bsp'))
+spice.furnsh(os.path.join(SPICE_PATH, 'de423.bsp'))
 
 sun = SpiceBody(spiceid='Sun')
 earth = SpiceBody(spiceid='Earth')
